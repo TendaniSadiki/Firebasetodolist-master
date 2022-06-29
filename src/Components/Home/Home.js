d8b1d845268f16850dcecc65baacd03a77c9bb71
Edmond Sadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-06-29 16:48:11 +0200

    {sign out and auth change} added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,7 +5,7 @@ import {CgMathPlus} from "react-icons/cg";
 import {CgCheck} from "react-icons/cg";
 import "./home.css";
 
-import { signOut, onAuthStateChanged } from "firebase/auth";
+
 import { auth, db } from "../../firebase-config";
 
 import { uid } from "uid";
@@ -88,7 +88,43 @@ return(
             <ul>
             <h3>My To Do List</h3>
                 <div>
-      
+        <div>
+            <input className="todo" type="text" placeholder="Enter inputs"
+            value={todo}
+            onChange={(text) => {
+              setTodo(text.target.value);
+            }}
+            ></input>
+            <button  value={todo}
+        onChange={(e) => setTodo(e.target.value)}>Add To List</button>
+            <br></br>
+            <div className="dateWrapper">
+            <div className="datesContent">
+            <label>Start date</label>
+            <input type="date" 
+            min="2022-06-22"
+            id="date"
+            value={startDate}
+            onChange={(text) => {
+              setStartDate(text.target.value);
+            }}
+            >
+            </input>
+            </div>
+            <br></br>
+            <div className="datesContent">
+            <label>Expiring date</label>
+            <input type="date"  
+            id="date"
+            value={expireDate}
+            onChange={(text) => {
+              setExDate(text.target.value);
+            }}
+            >
+            </input>
+            </div>
+            </div>
+        </div>
   {todos.length > 0 ? ( 
   < div>
       {todos.map((todo) => (
